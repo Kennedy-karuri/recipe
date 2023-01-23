7e8906164f59816efa3b54a78774106162827d14
Kennedy-karuri <kennkaruri99@gmail.com>
2023-01-23 13:29:22 +0300

    created new php.files

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
     <title>Recipe</title>
 </head>
 <body>
-    
+    <div class="homepage">
+        <h1>WELCOME TO KEN'S SPECIAL RECIPIES</h1>
+        <p>Food at its best.</p>
+        <p>Login or create an account with us to view our recipies</p>
+
+       </div>
 </body>
 </html>
