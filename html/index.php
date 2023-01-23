dc9ba563a20cccc198142dc51e79204d73d64940
Kennedy-karuri <kennkaruri99@gmail.com>
2023-01-23 13:53:26 +0300

    created login.form

--- html/index.php
+++ html/index.php
@@ -11,8 +11,66 @@
     <div class="homepage">
         <h1>WELCOME TO KEN'S SPECIAL RECIPIES</h1>
         <p>Food at its best.</p>
-        <p>Login or create an account with us to view our recipies</p>
-    <img src="" alt="">
+       
+
+        <?php
+
+
+if (!isset($_SESSION['username'])) {
+    echo "Please login first <a href='login.html'> here </a>";
+    exit;
+}
+
+if (isset($_GET['pageNumber']))
+    $menuChoice = $_GET['pageNumber'];
+else 
+    $menuChoice = 3;
+
+if ( $_SESSION['username']) {
+    require_once 'showTopMenu.php';
+   
+    switch ($menuChoice) {
+        case 1:
+            require_once 'showSearchForm.php';    
+            break;
+        case 2:
+            require_once 'showAddForm.php';
+            break;
+        case 3:
+            require_once 'showMainWelcome.php';
+            break;
+            case 4:
+                require_once 'ShowAdminPage.php';
+                break; 
+                case 5:
+                    require_once 'ShowUserAdminPage.php';
+                    break;  
+                    case 6:
+                        require_once 'ShowCommentsAdminPage.php';
+                        break;      
+              
+               
+    } 
+}
+else {
+    echo "<h1>Recipe Lister</h1>";
+    echo "Please login first <a href='login.html'>Login here</a>";
+    exit;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+ 
 
        </div>
 </body>
